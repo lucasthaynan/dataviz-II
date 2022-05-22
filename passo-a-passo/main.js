8f46dd217099e3632a6eb5440d6facc83419b297
Lucas Thaynan <lucasthaynan.mcz@gmail.com>
2022-05-22 19:55:17 -0300

    adicionando textos em cada etapa

--- passo-a-passo/main.js
+++ passo-a-passo/main.js
@@ -11,13 +11,20 @@ document.querySelector('button.proximo').addEventListener('click', e => {
     console.log(e)
     console.log(etapaVisualizacao)
 
+    let textos = document.querySelectorAll('div.texto > div')
+    console.log(textos)
+
+    textos.forEach(texto => texto.classList.remove('ativo'));
+
     if (etapaVisualizacao == 1) {
 
         document.querySelectorAll('span.instagram > div').forEach(elementoDiv => {
             elementoDiv.style.opacity = 1;
         }) 
 
-        document.querySelector('span.instagram').classList.add('ativo')         
+        document.querySelector('span.instagram').classList.add('ativo')      
+        
+        document.querySelector('.texto-instagram').classList.add('ativo')
         
     } else if (etapaVisualizacao == 2) {
 
@@ -26,6 +33,8 @@ document.querySelector('button.proximo').addEventListener('click', e => {
         }) 
 
         document.querySelector('span.whatsapp').classList.add('ativo') 
+        document.querySelector('.texto-whatsapp').classList.add('ativo')
+
     } else if (etapaVisualizacao == 3) {
 
         document.querySelectorAll('span.chrome > div').forEach(elementoDiv => {
@@ -33,6 +42,8 @@ document.querySelector('button.proximo').addEventListener('click', e => {
         }) 
 
         document.querySelector('span.chrome').classList.add('ativo') 
+
+        document.querySelector('.texto-chrome').classList.add('ativo')
 
     } else if (etapaVisualizacao == 4) {
 
@@ -42,6 +53,8 @@ document.querySelector('button.proximo').addEventListener('click', e => {
 
         document.querySelector('span.twitter').classList.add('ativo') 
 
+        document.querySelector('.texto-twitter').classList.add('ativo')
+
     } else if (etapaVisualizacao == 5) {
 
         document.querySelectorAll('span.free-fire > div').forEach(elementoDiv => {
@@ -50,6 +63,8 @@ document.querySelector('button.proximo').addEventListener('click', e => {
 
         document.querySelector('span.free-fire').classList.add('ativo') 
 
+        document.querySelector('.texto-free-fire').classList.add('ativo')
+
     } else if (etapaVisualizacao == 6) {
 
         document.querySelectorAll('span.outros-apps > div').forEach(elementoDiv => {
@@ -57,6 +72,7 @@ document.querySelector('button.proximo').addEventListener('click', e => {
         }) 
 
         document.querySelector('span.outros-apps').classList.add('ativo') 
+        document.querySelector('.texto-outros-apps').classList.add('ativo')
     }
 
     etapaVisualizacao += 1
